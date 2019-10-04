91085189f0aa2270445d1e8e446fbc9026fb2512
BPitts8019 <bradleyj.pitts@gmail.com>
2019-10-04 15:33:52 -0700

    Ready to add new components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+/*
+// 20191004153000
+// https://api.github.com/users/BPitts8019
 
+{
+   "login": "BPitts8019",
+   "id": 31142450,
+   "node_id": "MDQ6VXNlcjMxMTQyNDUw",
+   "avatar_url": "https://avatars3.githubusercontent.com/u/31142450?v=4",
+   "gravatar_id": "",
+   "url": "https://api.github.com/users/BPitts8019",
+   "html_url": "https://github.com/BPitts8019",
+   "followers_url": "https://api.github.com/users/BPitts8019/followers",
+   "following_url": "https://api.github.com/users/BPitts8019/following{/other_user}",
+   "gists_url": "https://api.github.com/users/BPitts8019/gists{/gist_id}",
+   "starred_url": "https://api.github.com/users/BPitts8019/starred{/owner}{/repo}",
+   "subscriptions_url": "https://api.github.com/users/BPitts8019/subscriptions",
+   "organizations_url": "https://api.github.com/users/BPitts8019/orgs",
+   "repos_url": "https://api.github.com/users/BPitts8019/repos",
+   "events_url": "https://api.github.com/users/BPitts8019/events{/privacy}",
+   "received_events_url": "https://api.github.com/users/BPitts8019/received_events",
+   "type": "User",
+   "site_admin": false,
+   "name": "Bradley Pitts",
+   "company": null,
+   "blog": "",
+   "location": "Phoenix, AZ",
+   "email": null,
+   "hireable": null,
+   "bio": null,
+   "public_repos": 44,
+   "public_gists": 0,
+   "followers": 1,
+   "following": 3,
+   "created_at": "2017-08-18T19:49:32Z",
+   "updated_at": "2019-09-18T02:00:56Z"
+}
+*/
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+      </div>
+   );
 }
 
 export default App;
